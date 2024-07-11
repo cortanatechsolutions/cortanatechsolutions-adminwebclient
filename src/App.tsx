276bf0c18de6f8018b41f3799d44ceecae4a9a08
faithfulcoronel <faithful.e.v.coronel@gmail.com>
2024-07-10 20:06:59 -0500

    reverted react router change

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import KTComponent from "./metronic/core";
 import { useEffect } from "react";
 import KTLayout from "./metronic/app/layouts/demo1.js";
 import SearchModal from "./components/search/SearchModal.tsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/dashboards/maindashboard.tsx";
 import SecondDashboard from "./pages/dashboards/seconddashboard.tsx";
 import ErrorPage404 from "./pages/errors/404.tsx";
@@ -25,18 +25,13 @@ function App() {
           <main className="grow content pt-5" id="content" role="content">
             <div className="container-fixed" id="content_container"></div>
             <div className="container-fixed">
-              <Router>
-                <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route
-                    path="/secondDashboard"
-                    element={<SecondDashboard />}
-                  />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/secondDashboard" element={<SecondDashboard />} />
 
-                  {/* Catch all unmatched routes */}
-                  <Route path="*" element={<ErrorPage404 />} />
-                </Routes>
-              </Router>
+                {/* Catch all unmatched routes */}
+                <Route path="*" element={<ErrorPage404 />} />
+              </Routes>
             </div>
           </main>
           <Footer />
